538876071e8d9a8bf726bbf15f82e6e7ed798532
DanielLeBonific <100143965+DanielLeBonific@users.noreply.github.com>
2023-01-19 16:15:24 +0300

    progress

--- app/src/main/java/com/example/park/MainActivity.kt
+++ app/src/main/java/com/example/park/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.park
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.park.R
 import com.example.park.setData
@@ -17,6 +19,5 @@ class MainActivity : AppCompatActivity() {
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN }
         my_recycler.adapter=Adapter(setData.SetPlanets())
         my_recycler.layoutManager=LinearLayoutManager(this)
-
     }
 }
--- app/src/main/java/com/example/park/ParkDetail.kt
+++ app/src/main/java/com/example/park/ParkDetail.kt
@@ -1,8 +1,11 @@
 package com.example.park
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 import com.example.park.ParkData
 import com.example.park.R
 import kotlinx.android.synthetic.main.activity_park_detail.*
@@ -10,19 +13,29 @@ import kotlinx.android.synthetic.main.activity_park_detail.*
 import kotlinx.android.synthetic.main.view.*
 
 class ParkDetail : AppCompatActivity() {
+    private lateinit var button: Button
     private lateinit var obj: ParkData
     private var parkImg:Int?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_park_detail)
+        button = findViewById(R.id.shareButton)
+        val url = "https://github.com/DanielLeBonific"
+        button.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra("Share this", url)
+            val chooser = Intent.createChooser(intent,"Share using...")
+            startActivity(chooser)
+        }
         window.decorView.apply { systemUiVisibility=
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN }
+
+
 
         obj= intent.getParcelableExtra("park")!!
         parkImg=intent.getIntExtra("parkImage",-1)
         setData(obj, parkImg!!)
-
-
     }
     private fun setData(obj:ParkData,parkImg:Int)
     {
@@ -35,3 +48,4 @@ class ParkDetail : AppCompatActivity() {
 
     }
 }
+
